86e3a79be600b5a4fa64cd79447e79230aa3e51c
Theophilus131 <theophilusumar27@gmail.com>
2025-05-05 12:58:57 +0100

    array of number

---
+++ ArrayOfNumber.java
@@ -0,0 +1,26 @@
+	public class ArrayOfNumber{
+	public static void main(String [] args){
+
+int [] number = new int [5];
+
+ 	number [0]=  22;
+	number [1]=  51;
+	number [2] = 38;
+	number [3]=  23;
+	number [4] = 48;
+
+System.out.println(" " + number[0]);
+System.out.println(" " + number[1]);
+System.out.println(" " + number[2]);
+System.out.println(" " + number[3]);
+System.out.println(" " + number[4]);
+
+System.out.println();
+
+System.out.printf("%d %d %d %d %d ", number[0], number[1], number[2], number[3], number[4]);
+
+
+
+
+}
+}
