6fb866f70562b3b005882e5918c0350e945a6081
Theophilus131 <theophilusumar27@gmail.com>
2025-05-06 15:12:49 +0100

    sum of number with do while loop completed

---
+++ SumDowhile.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class SumDowhile{
+public static void main(String [] args){
+
+ Scanner input = new Scanner(System.in);
+ int sum = 0;
+ int terminate;
+  
+
+ do{
+  System.out.println("Enter two number: ");
+  int firstNumber = input.nextInt();
+  int secondNumber = input.nextInt();
+ 
+     sum= firstNumber + secondNumber;
+   System.out.println("The sum is: " + sum);
+   
+ 
+   System.out.print(" Do you wish to perform this tasks again or 	enter 0 to end the operation");
+   	terminate = input.nextInt();
+ 
+
+  }while( terminate != 0);
+
+   System.out.println("Program ended.");
+	
+        
+
+	}
+
+
+
+}
